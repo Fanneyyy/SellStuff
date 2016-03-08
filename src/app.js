3e83b010300bb42a5672d90011dda6ffb43b5216
Fanneyyy <fanneyyyy@gmail.com>
2016-03-08 12:28:03 +0000

    Angular translate for sellers

--- src/app.js
+++ src/app.js
@@ -11,33 +11,9 @@ angular.module("project3App", ["ngRoute", "ui.bootstrap", "sharedServices", "pas
 		'en_*': 'en',
 		'is_*': 'is'
 	});
-	// $translateProvider.useStaticFilesLoader({
-	// 	prefix: "lang_",
-	// 	suffix: ".json"
-	// });
-	$translateProvider.translations('en', {
-		"sellers" : {
-			"Title":                     "Sellers List",
-			"Name":                      "Name",
-			"Category": 				 "Category", 
-			"Messages" : {
-				"LoadFailed":            "Failed to load the list of sellers",
-				"SaveSucceeded":         "The seller has been added successfully",
-				"SaveFailed":            "Unfortunately we could not add a new seller"
-			}
-		}
-	});
-	$translateProvider.translations('is', {
-		"sellers" : {
-			"Title":                     "Seljendur",
-			"Name":                      "Nafn",
-			"Category": 				 "Flokkur", 
-			"Messages" : {
-				"LoadFailed":            "Ekki tókst að sækja lista yfir seljendur",
-				"SaveSucceeded":         "Nýjum seljanda hefur verið bætt við",
-				"SaveFailed":            "Því miður tókst ekki að bæta við nýjum seljanda"
-			}
-		}
+	$translateProvider.useStaticFilesLoader({
+		prefix: "lang_",
+		suffix: ".json"
 	});
 	$translateProvider.useSanitizeValueStrategy('escape');
 	$translateProvider.preferredLanguage('en');
