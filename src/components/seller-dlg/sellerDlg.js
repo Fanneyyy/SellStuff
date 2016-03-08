a799d30f3882d236ce42e12e5e86832fd046264a
Fanneyyy <fanneyyyy@gmail.com>
2016-03-08 12:53:57 +0000

    Starting on Dialog

---
+++ src/components/seller-dlg/sellerDlg.js
@@ -0,0 +1,11 @@
+// "use strict";
+
+// angular.module("project3App").factory("SellerDlg", 
+// 	function SellerDlg($uibModal) {
+
+// 		return {
+// 			show: function() {
+// 				var modal
+// 			}
+// 		}
+// });
