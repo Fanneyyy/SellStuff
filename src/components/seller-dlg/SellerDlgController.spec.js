f238a0d16b28dd153cb44d36d848ade1ddd58d34
Kristinn Þorri <kristinn.thorri@gmail.com>
2016-03-09 15:00:45 +0000

    added testing for sellerdlgcontroller

--- src/components/seller-dlg/SellerDlgController.spec.js
+++ src/components/seller-dlg/SellerDlgController.spec.js
@@ -4,15 +4,37 @@ describe("SellerDlgController should be unit tested here", function() {
 
 	var scope;
 
+	var mockSeller = {
+		id: 1,
+		name: 'Sveinn Andri',
+		category: 'Grænmeti',
+		imagePath: 'http://www.visindavefur.is/myndir/graenmeti_030308.jpg'
+	}
+
+
 	beforeEach(module("project3App"));
 
 	beforeEach(inject(function ($rootScope, $controller) {
 		scope = $rootScope.$new();
+		scope.$close = function(seller){};
+		scope.$dismiss = function(){};
 
 		var SellerDlgController = $controller('SellerDlgController', {
 			$scope: scope,
+			currentSeller: mockSeller,
 		});
 
+	    spyOn(scope, "$close");
+	   	spyOn(scope, "$dismiss");
+
 	}));
+	it('should call the function scope.close with mockSeller', function() {
+		scope.onOk();
+		expect(scope.$close).toHaveBeenCalledWith(mockSeller);
+	});
+	it('should call the function scope.dismiss', function() {
+		scope.onCancel();
+		expect(scope.$dismiss).toHaveBeenCalled();
+	});
 // TODO: Need to find how to use currentSeller for this controller
 });
