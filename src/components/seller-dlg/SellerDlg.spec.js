e52358e4f12c6d86429ee3e0054b65c618c6aaed
Kristinn Þorri <kristinn.thorri@gmail.com>
2016-03-10 10:24:00 +0000

    testing

---
+++ src/components/seller-dlg/SellerDlg.spec.js
@@ -0,0 +1,25 @@
+describe('SellerDlgController should be unit tested here', function () {
+ 
+	var mockSeller = {
+		id: 1,
+		name: 'Sveinn Andri',
+		category: 'Grænmeti',
+		imagePath: 'http://www.visindavefur.is/myndir/graenmeti_030308.jpg'
+	}
+
+  	beforeEach(module('project3App'));
+  	beforeEach(module(function ($provide) {
+    	$provide.value('oneOfMyOtherServicesStub', {
+        	someVariable: 1
+    	});
+  	}));
+
+  	it('can get an instance of SellerDlg', inject(function(SellerDlg) {
+    	expect(SellerDlg).toBeDefined();
+  	}));
+
+  	it('SellerDlg.show should be defined', inject(function(SellerDlg) {
+  		SellerDlg.show(mockSeller);
+  		expect(SellerDlg).toBeDefined();
+  	}));
+});
--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -10,12 +10,13 @@ describe("SellersController should be unit tested here", function() {
 		scope = $rootScope.$new();
 
 		var SellersController = $controller('SellersController', {
-			$scope: scope,
+			$scope: scope
 		});
 
 	}));
 
-	it('should declare the function sortColumns', function() {
+	it('should declare sortColumns', function() {
+		debugger;
 		expect(scope.sortColumns).toBeDefined();
 	});
 });
