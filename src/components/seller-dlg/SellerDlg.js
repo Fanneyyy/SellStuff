e03087913b5ae3a5e43d061ba40707b0f2b1e6ed
kthorri <kristinn.thorri@gmail.com>
2016-03-11 09:13:22 +0000

    adding centris notify and converting indentation

--- src/components/seller-dlg/SellerDlg.js
+++ src/components/seller-dlg/SellerDlg.js
@@ -1,22 +1,21 @@
 "use strict";
 
 angular.module("project3App").factory("SellerDlg", 
-	function SellerDlg($uibModal) {
+    function SellerDlg($uibModal) {
 
-		return {
-			show: function(object) {
-				var modalInstance = $uibModal.open({
-					animation: true,
-					templateUrl: "components/seller-dlg/sellerDlg.html",
-					controller: "SellerDlgController",
-					resolve: {
-				        currentSeller: function () {
-				          return object;
-				        }
-				    }
-				});
-
-				return modalInstance.result;
-			}
-		};
+        return {
+            show: function(object) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: "components/seller-dlg/sellerDlg.html",
+                    controller: "SellerDlgController",
+                    resolve: {
+                        currentSeller: function () {
+                            return object;
+                        }
+                    }
+                });
+                return modalInstance.result;
+            }
+        };
 });
--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -2,43 +2,42 @@
 
 angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource, SellerDlg, centrisNotify) {
-	$scope.sortType = 'name'; // set the default sort type
-  	$scope.sortReverse = false;  // set the default sort order
+    $scope.sortType = 'name'; // set the default sort type
+    $scope.sortReverse = false;  // set the default sort order
 
-  	$scope.sortColumns = function sortColumns(category) {
-  		$scope.sortType = category;
-  		$scope.sortReverse = !$scope.sortReverse;
-  	};
+    $scope.sortColumns = function sortColumns(category) {
+        $scope.sortType = category;
+        $scope.sortReverse = !$scope.sortReverse;
+    };
 
-	AppResource.getSellers().success(function(sellers) {
-		$scope.sellers = sellers;
-	});
+    AppResource.getSellers().success(function(sellers) {
+        $scope.sellers = sellers;
+    });
 
-	$scope.onAddSeller = function onAddSeller() {
+    $scope.onAddSeller = function onAddSeller() {
 
-		SellerDlg.show().then(function(seller) {
-			AppResource.addSeller(seller).success(function (seller) {
-				centrisNotify.success(seller.name + " has been successfully added.");
-			}).error(function() {
-				centrisNotify.error("The seller will not be added due to error.");
-			});
-		}, function() {
-			centrisNotify.info("The seller will not be added.");
-		});
-	};
+        SellerDlg.show().then(function(seller) {
+            AppResource.addSeller(seller).success(function (seller) {
+                centrisNotify.success(seller.name + " has been successfully added.");
+            }).error(function() {
+                centrisNotify.error("The seller will not be added due to error.");
+            });
+        }, function() {
+            centrisNotify.info("The seller will not be added.");
+        });
+    };
 
-	$scope.onEditSeller = function onEditSeller(s) {
-		var oldSeller = $.extend({}, s);
-		SellerDlg.show(oldSeller).then(function(seller) {
+    $scope.onEditSeller = function onEditSeller(s) {
+        var oldSeller = $.extend({}, s);
+        SellerDlg.show(oldSeller).then(function(seller) {
 
-			AppResource.updateSeller(oldSeller.id, seller).success(function (seller) {
-				centrisNotify.success(seller.name + " has been successfully edited.");
+            AppResource.updateSeller(oldSeller.id, seller).success(function (seller) {
+                centrisNotify.success(seller.name + " has been successfully edited.");
 
-				console.log("Seller updated");
-			}).error(function() {
-				centrisNotify.error(oldSeller.name + "was not edited.");
-			});
+            }).error(function() {
+                centrisNotify.error(oldSeller.name + "was not edited.");
+            });
 
-		});
-	};
+        });
+    };
 });
